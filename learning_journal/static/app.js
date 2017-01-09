9981f67dc2cab5b1c4c0cdacaa7c10adff2e249d
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2017-01-08 16:45:50 -0800

    commenting out ajax until I fix it, fixing bug in html tags on list.jinja2

--- learning_journal/static/app.js
+++ learning_journal/static/app.js
@@ -12,35 +12,35 @@ $(document).ready(function(){
 });
 
 // AJAX to enable asynchronous reload for create form
-$(document).ready(function(){
-    var csrfToken = "{{ request.session.get_csrf_token() }}";
-    $('.home_form').submit(function(e){
-        e.preventDefault();
-        $.post({
-            headers: { 'X-CSRF-Token': csrfToken },
-            url: 'create',
-            data: {"title": $('#title').val(), "post": $('#post').val()},
-            success: function(){
-                var creation_date = new Date();
-                var year = creation_date.getFullYear();
-                var month = creation_date.getMonth() + 1;
-                if (month.toString().length == 1) {
-                    month = "0" + month;
-                };
-                var day = creation_date.getDate();
-                if (day.toString().length == 1){
-                    day = "0" + day;
-                };
+// $(document).ready(function(){
+//     var csrfToken = "{{ request.session.get_csrf_token() }}";
+//     $('.home_form').submit(function(e){
+//         e.preventDefault();
+//         $.post({
+//             headers: { 'X-CSRF-Token': csrfToken },
+//             url: 'create',
+//             data: {"title": $('#title').val(), "post": $('#post').val()},
+//             success: function(){
+//                 var creation_date = new Date();
+//                 var year = creation_date.getFullYear();
+//                 var month = creation_date.getMonth() + 1;
+//                 if (month.toString().length == 1) {
+//                     month = "0" + month;
+//                 };
+//                 var day = creation_date.getDate();
+//                 if (day.toString().length == 1){
+//                     day = "0" + day;
+//                 };
 
-                $('#entry_title').append($('#title').val());
-                $('#entry_date').append($('creation_date'));
-                $('#entry_text').append($('#post').val());
-                $('#title').val("");
-                $('#entry').val("");
-            }
-        });
-    });
-});  
+//                 $('#entry_title').append($('#title').val());
+//                 $('#entry_date').append($('creation_date'));
+//                 $('#entry_text').append($('#post').val());
+//                 $('#title').val("");
+//                 $('#entry').val("");
+//             }
+//         });
+//     });
+// });  
 
 // AJAX to enable asynchronous reload for edit view-- NOT SETUP TO RUN
 $(document).ready(function(){
